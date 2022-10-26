7c8e4c407473740c42efcbfd7d5ee3fdab496877
Obed Mikombo <omikombo@uncc.edu>
2022-10-26 07:45:21 -0400

    flask app #3 - added new note route an d view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -12,13 +12,10 @@ app = Flask(__name__)  # create an app
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/')
-@app.route('/notes/<note_id>')
-def get_notes(note_id):
-    notes = {1: {'title': 'First Name', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
-
-    return render_template('note.html', note=notes[int(note_id)])
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Obed', 'email': 'omikombo@uncc.edu'}
+    return render_template('new.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
