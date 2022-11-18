1eec0a7fcd3c931631027ea8e2f5915b6053c034
mikombo7obed <108364225+mikombo7obed@users.noreply.github.com>
2022-11-18 12:57:25 -0500

    Update tests.py

--- MVP/tests.py
+++ MVP/tests.py
@@ -8,28 +8,28 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>Use this site to maintain and organize your projects.</h2>' in response.text, True)
 
-    def test_notes(self):
-        response = requests.get("http://127.0.0.1:5000/notes")
+    def test_projects(self):
+        response = requests.get("http://127.0.0.1:5000/projects")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         self.assertEqual('Title' and 'Date' in response.text, True)
 
-    def test_note(self):
-        response = requests.get("http://127.0.0.1:5000/notes/1")
+    def test_project(self):
+        response = requests.get("http://127.0.0.1:5000/projects/1")
         statuscode = response.status_code
         self.assertEqual(statuscode, 404)
-        self.assertEqual('First Note' in response.text, False)
+        self.assertEqual('First Project' in response.text, False)
 
-    def test_new(self):
-        response = requests.get("http://127.0.0.1:5000/notes/new")
+    def test_newProject(self):
+        response = requests.get("http://127.0.0.1:5000/projects/new")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         self.assertEqual('<form action="new" method="post">' in response.text, True)
 
-    def test_delete(self):
-        response = requests.get('http://127.0.0.1:5000/notes/delete')
+    def test_deleteProject(self):
+        response = requests.get('http://127.0.0.1:5000/projects/delete')
         statuscode = response.status_code
         self.assertEqual(statuscode, 404)
 
