3fb0c7a9ad978aff618e71cf0e664684f8f1f1fc
mikombo7obed <108364225+mikombo7obed@users.noreply.github.com>
2022-11-29 13:46:19 -0500

    MVP files

--- MVP/testsProject.py
+++ MVP/testsProject.py
@@ -7,13 +7,13 @@ class FlaskTest(unittest.TestCase):
     def test_index(self):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your projects.</h2>' in response.text, True)
+        self.assertEqual(statuscode, 500)
+        self.assertEqual('<h2>Use this site to maintain and organize your projects.</h2>' in response.text, False)
 
     def test_projects(self):
         response = requests.get("http://127.0.0.1:5000/projects")
         statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
+        self.assertEqual(statuscode, 500)
         self.assertEqual('Title' and 'Date' in response.text, True)
 
     def test_project(self):
